64e0cdb6a8e3dac23bb705f99f79331144c09e78
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-02-17 10:57:14 +1300

    feat: crdt

---
+++ src/crdt/crdt.spec.ts
@@ -0,0 +1,122 @@
+import { describe, expect, it, vitest } from "vitest";
+import { createCRDT } from ".";
+
+describe("crdt", () => {
+	describe("`dispatch`", () => {
+		it("invokes `onChange` when updates are dispatched", () => {
+			const INITIAL_VALUE = { b: 1 };
+			const onChange = vitest.fn();
+			const { dispatch } = createCRDT({
+				initialValue: INITIAL_VALUE,
+				onChange,
+			});
+
+			const FIRST_UPDATE = { a: 1 };
+			dispatch(FIRST_UPDATE);
+
+			expect(onChange).toBeCalledWith({
+				...INITIAL_VALUE,
+				...FIRST_UPDATE,
+			});
+			expect(onChange).toBeCalledTimes(1);
+		});
+
+		it("allows an updates to be computed", () => {
+			const setState = vitest.fn();
+			const crdt = createCRDT({
+				initialValue: Object.create(null),
+				onChange: setState,
+			});
+
+			const FIRST_UPDATE = { a: 1 };
+			const SECOND_UPDATE = { b: 1 };
+
+			crdt.dispatch(previous => ({ ...previous, ...FIRST_UPDATE }));
+
+			expect(setState).toBeCalledWith(FIRST_UPDATE);
+			expect(setState).toBeCalledTimes(1);
+
+			crdt.dispatch(previous => ({ ...previous, ...SECOND_UPDATE }));
+
+			expect(setState).toBeCalledWith({
+				...FIRST_UPDATE,
+				...SECOND_UPDATE,
+			});
+			expect(setState).toBeCalledTimes(2);
+
+			expect(crdt.data).toEqual({ ...FIRST_UPDATE, ...SECOND_UPDATE });
+		});
+
+		it("allows updates to be specified", () => {
+			const upsert = vitest.fn().mockImplementation(updates => {
+				const query = `INSERT INTO users(${Object.keys(updates).join(", ")})
+					VALUES (${Object.values(updates).join(", ")})
+					ON CONFLICT (uuid)
+					DO UPDATE SET
+						${Object.entries(updates)
+							.map(([key, value]) => `${key} = ${value}`)
+							.join(", ")};
+				`;
+
+				return query;
+			});
+			const { dispatch } = createCRDT({
+				initialValue: Object.create(null),
+				onChange: upsert,
+			});
+
+			const FIRST_UPDATE = { a: 1 };
+			dispatch(FIRST_UPDATE);
+
+			expect(upsert).toBeCalledWith(FIRST_UPDATE);
+			expect(upsert).toBeCalledTimes(1);
+		});
+
+		it("returns a new reference", () => {
+			const onChange = vitest.fn();
+			const crdt = createCRDT({
+				initialValue: Object.create(null),
+				onChange,
+			});
+
+			const INITIAL_VALUE = crdt.data;
+			const FIRST_UPDATE = { a: 1 };
+
+			const latest = crdt.dispatch(FIRST_UPDATE);
+
+			const FINAL_VALUE = crdt.data;
+
+			expect(latest).not.toBe(INITIAL_VALUE);
+			expect(FINAL_VALUE).toBe(latest);
+		});
+
+		it("only applies the latest updates", () => {
+			const onChange = vitest.fn();
+			const crdt = createCRDT({
+				initialValue: Object.create(null),
+				onChange,
+			});
+
+			const FIRST_UPDATE = {
+				timestamp: new Date(0),
+				a: 1,
+			};
+
+			crdt.dispatch(FIRST_UPDATE);
+
+			expect(onChange).not.toBeCalled();
+			expect(crdt.data).toEqual(Object.create(null));
+		});
+	});
+
+	describe("`data`", () => {
+		it("has stable references", () => {
+			const crdt = createCRDT({
+				initialValue: Object.create(null),
+				onChange: vitest.fn(),
+			});
+
+			expect(crdt.data).toBe(crdt.data);
+		});
+	});
+});
