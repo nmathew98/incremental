3911e4439ac353cd6f3321a246ae18ba888970a4
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-02-17 20:00:21 +1300

    refactor: remove memo

--- src/memo/memo.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from "vitest";
-
-describe("makeMemo", () => {
-	it.todo("throws if `fn` is not named");
-
-	it.todo("memoizes the result of `fn`");
-
-	it.todo("uses a custom `resolver` if one is specified to obtain the key");
-});
