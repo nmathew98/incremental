52b482411b2308a69814e04dafaf6800e9b04df2
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-02-17 18:10:16 +1300

    0.0.2 (#15)
    * feat: crdt (#1)
    
    * feat: crdt
    
    * test: rename test for createCRDT
    
    * feat: monitor (#2)
    
    * feat: monitor
    
    * test: tests for makeMonitored
    
    * fix: make monitored fetchFn throw always when fetchFn throws (#3)
    
    * fix: forward args on success and error (#4)
    
    * feat: retry (#5)
    
    * chore: update README (#6)
    
    * fix: return onChange result and if timestamp is provided then that is the updatedDate, if not it is current Date (#7)
    
    * feat: memo (#8)
    
    * feat: supply onChange with previous value (#9)
    
    * test: update unit tests for crdt (#10)
    
    * types: types for crdt, memo and monitor, monitor types need to be improved (#12)
    
    * test: update unit test and accepting a Date again for timestamp (#13)
    
    * chore: update test tag in readme (#14)
    
    * 0.0.2
    
    * chore: remove memo/types.ts

--- src/crdt/types.ts
+++ src/crdt/types.ts
@@ -1,1 +1,29 @@
-export type TODO = any;
+export interface CreateCRDTParameters<T extends Record<string, any>> {
+	/**
+	 * The initial value of the CRDT
+	 */
+	initialValue: T;
+
+	/**
+	 * Return a value other than `null` or `undefined` for it to be returned by `dispatch`
+	 */
+	onChange: (next: T, previous: T) => any;
+
+	/**
+	 * Keep track of all versions for debugging
+	 */
+	trackVersions?: boolean;
+}
+
+export interface CRDT<T extends Record<string, any>> {
+	readonly data: T;
+	dispatch: <R = T>(
+		updates: Partial<T> & { timestamp?: Date },
+		options?: DispatchOptions<T>,
+	) => R;
+	versions: T[];
+}
+
+export interface DispatchOptions<T extends Record<string, any>> {
+	onChange?: (next: T, previous: T) => void;
+}
