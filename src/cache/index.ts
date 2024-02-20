102d65629828e476b9ffa5393f732f62d262b3df
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-02-20 17:31:12 +1300

    feat: caching (#40)
    * feat: caching
    
    * test: update unit tests
    
    * refactor: remove CacheProviderOptions, unused
    
    * chore: update github actions

---
+++ src/cache/index.ts
@@ -0,0 +1,50 @@
+import type { CacheProvider, CacheStore, CacheValue } from "./types";
+
+export const createCacheProvider = (
+	store: CacheStore = new WeakMap(),
+): CacheProvider => {
+	const prefetchedQueries = new Map();
+
+	const makeOnChange = (queryKey: WeakKey) => (next: CacheValue) => {
+		if (prefetchedQueries.has(queryKey)) prefetchedQueries.delete(queryKey);
+
+		store.set(queryKey, next);
+	};
+
+	const getCachedValue = (queryKey: WeakKey) => {
+		if (prefetchedQueries.has(queryKey)) {
+			const cachedValue = prefetchedQueries.get(queryKey);
+
+			store.set(queryKey, cachedValue);
+
+			return cachedValue;
+		}
+
+		if (store.has(queryKey)) return store.get(queryKey) ?? null;
+
+		return null;
+	};
+
+	const makeInvalidateCachedValue = (queryKey: WeakKey) => {
+		prefetchedQueries.delete(queryKey);
+		store.delete(queryKey);
+	};
+
+	const setCachedValue = (queryKey: WeakKey) => (next: CacheValue) =>
+		void prefetchedQueries.set(queryKey, next);
+
+	const clearCache = () => {
+		// Intentional mutation, `WeakMap` has no `clear`
+		store = new WeakMap();
+
+		prefetchedQueries.clear();
+	};
+
+	return {
+		makeOnChange,
+		getCachedValue,
+		setCachedValue,
+		makeInvalidateCachedValue,
+		clearCache,
+	};
+};
