139c2d1bac0c0197c34a929a2986abee7c20e708
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-02-20 17:42:54 +1300

    2.0.1 (#42)
    * feat: crdt (#1)
    
    * feat: crdt
    
    * test: rename test for createCRDT
    
    * feat: monitor (#2)
    
    * feat: monitor
    
    * test: tests for makeMonitored
    
    * fix: make monitored fetchFn throw always when fetchFn throws (#3)
    
    * fix: forward args on success and error (#4)
    
    * feat: retry (#5)
    
    * chore: update README (#6)
    
    * fix: return onChange result and if timestamp is provided then that is the updatedDate, if not it is current Date (#7)
    
    * feat: memo (#8)
    
    * feat: supply onChange with previous value (#9)
    
    * test: update unit tests for crdt (#10)
    
    * types: types for crdt, memo and monitor, monitor types need to be improved (#12)
    
    * test: update unit test and accepting a Date again for timestamp (#13)
    
    * chore: update test tag in readme (#14)
    
    * 0.0.2
    
    * types: improved type checks for crdt and monitor (#16)
    
    * refactor: remove memo (#17)
    
    * 0.0.3
    
    * feat: add onSuccess and onError (#20)
    
    * feat: allow skipping crdt onChange if isPersisted is specified, dispatch onChange always runs if specified. also removed timestamp, i think it will be unnecessary, subscriptions are not received from a queue (#21)
    
    * feat: adding immer for simple change tracking (#22)
    
    * chore: update readme (#23)
    
    * chore: update readme
    
    * test: add additional test for arrays
    
    * chore: more readme tweaks
    
    * test: additional expect
    
    * chore: update readme
    
    * docs: comments
    
    * docs: more comments
    
    * chore: update readme (#24)
    
    * 1.0.0
    
    * chore: add repository link (#26)
    
    * 1.0.1
    
    * feat: accept arrays (#28)
    
    * 1.0.2
    
    * types: extend onChange types for array, forgot to update (#30)
    
    * types: also extending for array for onSuccess and onError (#31)
    
    * feat: bring diff out of crdt and export as utility fn (#32)
    
    * feat: bring diff out of crdt and export as utility fn
    
    * docs: remove references to diff in crdt
    
    * refactor: cleanup diff (#33)
    
    * chore: update readme (#34)
    
    * 2.0.0
    
    * types: improve types for dispatcher (#36)
    
    * fix: dispatch new array (#37)
    
    * feat: caching (#40)
    
    * feat: caching
    
    * test: update unit tests
    
    * refactor: remove CacheProviderOptions, unused
    
    * chore: update github actions
    
    * fix: delete prefetched queries (#41)
    
    * 2.0.1

---
+++ src/cache/types.ts
@@ -0,0 +1,16 @@
+export interface CacheProvider {
+	makeOnChange: (queryKey: WeakKey) => (next: CacheValue) => void;
+	getCachedValue: (queryKey: WeakKey) => null | CacheValue;
+	makeInvalidateCachedValue: (queryKey: WeakKey) => void;
+	setCachedValue: (queryKey: WeakKey) => (next: CacheValue) => void;
+	clearCache: () => void;
+}
+
+export type CacheValue = Record<string | symbol | number, any> | unknown[];
+
+export interface CacheStore {
+	has: (queryKey: WeakKey) => boolean;
+	get: (queryKey: WeakKey) => void | CacheValue | Promise<CacheValue>;
+	set: (queryKey: WeakKey, value: void | CacheValue) => void;
+	delete: (queryKey: WeakKey) => void;
+}
