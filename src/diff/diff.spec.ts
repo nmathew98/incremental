c1d7b4f0cab64d5338a4af33dc15a7d0090e0447
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-02-21 19:50:12 +1300

    2.1.1 (#59)
    * feat: crdt (#1)
    
    * feat: crdt
    
    * test: rename test for createCRDT
    
    * feat: monitor (#2)
    
    * feat: monitor
    
    * test: tests for makeMonitored
    
    * fix: make monitored fetchFn throw always when fetchFn throws (#3)
    
    * fix: forward args on success and error (#4)
    
    * feat: retry (#5)
    
    * chore: update README (#6)
    
    * fix: return onChange result and if timestamp is provided then that is the updatedDate, if not it is current Date (#7)
    
    * feat: memo (#8)
    
    * feat: supply onChange with previous value (#9)
    
    * test: update unit tests for crdt (#10)
    
    * types: types for crdt, memo and monitor, monitor types need to be improved (#12)
    
    * test: update unit test and accepting a Date again for timestamp (#13)
    
    * chore: update test tag in readme (#14)
    
    * 0.0.2
    
    * types: improved type checks for crdt and monitor (#16)
    
    * refactor: remove memo (#17)
    
    * 0.0.3
    
    * feat: add onSuccess and onError (#20)
    
    * feat: allow skipping crdt onChange if isPersisted is specified, dispatch onChange always runs if specified. also removed timestamp, i think it will be unnecessary, subscriptions are not received from a queue (#21)
    
    * feat: adding immer for simple change tracking (#22)
    
    * chore: update readme (#23)
    
    * chore: update readme
    
    * test: add additional test for arrays
    
    * chore: more readme tweaks
    
    * test: additional expect
    
    * chore: update readme
    
    * docs: comments
    
    * docs: more comments
    
    * chore: update readme (#24)
    
    * 1.0.0
    
    * chore: add repository link (#26)
    
    * 1.0.1
    
    * feat: accept arrays (#28)
    
    * 1.0.2
    
    * types: extend onChange types for array, forgot to update (#30)
    
    * types: also extending for array for onSuccess and onError (#31)
    
    * feat: bring diff out of crdt and export as utility fn (#32)
    
    * feat: bring diff out of crdt and export as utility fn
    
    * docs: remove references to diff in crdt
    
    * refactor: cleanup diff (#33)
    
    * chore: update readme (#34)
    
    * 2.0.0
    
    * types: improve types for dispatcher (#36)
    
    * fix: dispatch new array (#37)
    
    * feat: caching (#40)
    
    * feat: caching
    
    * test: update unit tests
    
    * refactor: remove CacheProviderOptions, unused
    
    * chore: update github actions
    
    * fix: delete prefetched queries (#41)
    
    * 2.0.1
    
    * chore: export cache (#43)
    
    * 2.0.2
    
    * feat: createIncremental (#45)
    
    * feat: createIncremental
    
    * fix: initialValue of crdt should be crdtInitialValue
    
    * fix: adding optional clear field on CacheStore (#46)
    
    * fix: only assign to __cache__ if nullish (#47)
    
    * refactor: going with a map with weak ref values instead of WeakMap (#48)
    
    * refactor: updating types for incremental and CacheProvider (#49)
    
    * fix: makeInvalidateCachedValue should return a fn (#50)
    
    * refactor: make incremental sync (#51)
    
    * fix: getCachedValue and promises (#52)
    
    * feat: weak cache instead of bubbling WeakRef up to CacheProvider (#53)
    
    * refactor: simplify diff (#54)
    
    * refactor: remove refetchOnWindowFocus (#55)
    
    * 2.1.0
    
    * fix: diff condition previous does not have key or previous value is not the same (#57)
    
    * test: update unit tests for diff (#58)
    
    * 2.1.1

--- src/diff/diff.spec.ts
+++ src/diff/diff.spec.ts
@@ -3,71 +3,133 @@ import { diff } from ".";
 import { createCRDT } from "../crdt";
 
 describe("diff", () => {
-	it("diffs nested property changes based on references", () => {
-		const INITIAL_VALUE: Record<string, any> = {
-			a: {
-				b: {
-					c: {
-						d: 1,
+	describe("addition", () => {
+		it("diffs nested property changes based on references", () => {
+			const INITIAL_VALUE: Record<string, any> = {
+				a: {
+					b: {
+						c: {
+							d: 1,
+						},
+						e: {
+							f: 1,
+						},
 					},
-					e: {
-						f: 1,
-					},
+					g: 1,
 				},
-				g: 1,
-			},
-			h: 1,
-			i: {
-				j: {
-					k: 1,
-				},
-			},
-		};
-
-		const crdt = createCRDT({
-			initialValue: INITIAL_VALUE,
-			onChange: vitest.fn(),
-		});
-
-		crdt.dispatch(previousValue => {
-			previousValue.a.b.c = {
-				d: 2,
 				h: 1,
+				i: {
+					j: {
+						k: 1,
+					},
+				},
 			};
 
-			return previousValue;
+			const crdt = createCRDT({
+				initialValue: INITIAL_VALUE,
+				onChange: vitest.fn(),
+			});
+
+			crdt.dispatch(previousValue => {
+				previousValue.a.b.c = {
+					d: 2,
+					h: 1,
+				};
+
+				return previousValue;
+			});
+
+			expect(crdt.data.a.b.e).toBe(INITIAL_VALUE.a.b.e);
+			expect(diff(crdt.data, INITIAL_VALUE)).not.toEqual({
+				h: 1,
+				i: { j: { k: 1 } },
+			});
 		});
 
-		expect(crdt.data.a.b.e).toBe(INITIAL_VALUE.a.b.e);
-		expect(diff(crdt.data, INITIAL_VALUE)).not.toEqual({
-			h: 1,
-			i: { j: { k: 1 } },
+		it("diffs array value changes based on references", () => {
+			const INITIAL_VALUE: any[] = [{ a: 1 }, { b: 1 }, { c: 1 }];
+
+			const crdt = createCRDT({
+				initialValue: INITIAL_VALUE,
+				onChange: vitest.fn(),
+			});
+
+			crdt.dispatch(previousValue => [...previousValue, { d: 1 }]);
+
+			expect(diff(crdt.data, INITIAL_VALUE)).toEqual([{ d: 1 }]);
+		});
+
+		it("diffs array value changes based on values", () => {
+			const INITIAL_VALUE: number[] = [1, 2, 3];
+
+			const crdt = createCRDT({
+				initialValue: INITIAL_VALUE,
+				onChange: vitest.fn(),
+			});
+
+			crdt.dispatch(previousValue => void previousValue.push(4));
+
+			expect(diff(crdt.data, INITIAL_VALUE)).toEqual([4]);
 		});
 	});
 
-	it("diffs array value changes based on references", () => {
-		const INITIAL_VALUE: any[] = [{ a: 1 }, { b: 1 }, { c: 1 }];
+	describe("subtraction", () => {
+		it("arrays with primitive types", () => {
+			const INITIAL_VALUE: number[] = [1, 2, 3];
 
-		const crdt = createCRDT({
-			initialValue: INITIAL_VALUE,
-			onChange: vitest.fn(),
+			const crdt = createCRDT({
+				initialValue: INITIAL_VALUE,
+				onChange: vitest.fn(),
+			});
+
+			crdt.dispatch(previousValue => void previousValue.pop());
+
+			expect(diff(INITIAL_VALUE, crdt.data)).toEqual([3]);
 		});
 
-		crdt.dispatch(previousValue => [...previousValue, { d: 1 }]);
+		it("arrays with reference types", () => {
+			const LAST_ELEMENT = { c: 3 };
+			const INITIAL_VALUE = [{ a: 1 }, { b: 2 }, LAST_ELEMENT];
 
-		expect(diff(crdt.data, INITIAL_VALUE)).toEqual([{ d: 1 }]);
-	});
+			const crdt = createCRDT({
+				initialValue: INITIAL_VALUE,
+				onChange: vitest.fn(),
+			});
 
-	it("diffs array value changes based on values", () => {
-		const INITIAL_VALUE: number[] = [1, 2, 3];
+			crdt.dispatch(previousValue => void previousValue.pop());
 
-		const crdt = createCRDT({
-			initialValue: INITIAL_VALUE,
-			onChange: vitest.fn(),
+			expect(diff(INITIAL_VALUE, crdt.data)).toEqual([LAST_ELEMENT]);
 		});
 
-		crdt.dispatch(previousValue => void previousValue.push(4));
+		it("if a nested property is removed, its entire root is different", () => {
+			const INITIAL_VALUE: Record<string, any> = {
+				a: {
+					b: {
+						c: 1,
+					},
+				},
+				d: 2,
+				e: {
+					f: {
+						g: {
+							h: 1,
+						},
+					},
+				},
+			};
 
-		expect(diff(crdt.data, INITIAL_VALUE)).toEqual([4]);
+			const crdt = createCRDT({
+				initialValue: INITIAL_VALUE,
+				onChange: vitest.fn(),
+			});
+
+			crdt.dispatch(previousValue => {
+				delete previousValue.a.b.c;
+			});
+
+			expect(diff(INITIAL_VALUE, crdt.data)).toEqual({
+				a: { b: { c: 1 } },
+			});
+		});
 	});
 });
